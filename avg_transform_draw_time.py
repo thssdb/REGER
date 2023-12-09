22b39b6d0d3f24ac34622014feb1142a3b3bf5f7
xjz17 <67282793+xjz17@users.noreply.github.com>
2023-12-09 10:13:38 +0800

    update

---
+++ avg_transform_draw_time.py
@@ -0,0 +1,116 @@
+import os
+import pandas as pd
+
+dir_r = ["EPM-Education","GW-Magnetic","Metro-Traffic", "Nifty-Stocks", "USGS-Earthquakes", 
+         "CS-Sensors", "Cyber-Vehicle", "TH-Climate", "TY-Fuel","TY-Transport","FANYP-Sensors","TRAJET-Transport"]
+
+path_ratio = './compression_ratio/sota_ratio/' 
+path_rr_ratio =  './compression_ratio/elf/'  
+path_remove_value =  './compression_ratio/reger_float/'  
+
+df_result = pd.DataFrame(columns=['Encoding','Dataset','Encoding Time'])
+result_count = 0
+
+for j in range(len(dir_r)):
+    dir_ratio = path_ratio + dir_r[j] + "_ratio.csv"
+    df = pd.read_csv(dir_ratio)
+    for k in range(df.shape[0]):
+        df.loc[k,"Encode"] = df.iloc[k,4] / df.iloc[k,6]
+    df_avg = df.groupby(by = df['Encoding Algorithm'], as_index=False).agg(lambda x: x.mean() if pd.api.types.is_numeric_dtype(x) else x)
+    len_6_df = df_avg.shape[0]
+
+
+    dir_ratio_rr = path_rr_ratio + dir_r[j] + "_ratio.csv"
+    df_rr = pd.read_csv(dir_ratio_rr)
+    df_rr['Encoding Algorithm'] = df_rr['Encoding Algorithm'].replace('ElfCompressor', 'Elf')
+    for k in range(df_rr.shape[0]):
+        df_rr.loc[k,"Encode"] = df_rr.iloc[k,2] / 1000 #df_rr.iloc[k,4] 
+    df_avg_rr = df_rr.groupby(by = df_rr['Encoding Algorithm'], as_index=False).agg(lambda x: x.mean() if pd.api.types.is_numeric_dtype(x) else x)
+    len_rr_df = df_avg_rr.shape[0]
+
+
+    dir_ratio_pfor = path_remove_value + dir_r[j] + "_ratio.csv"
+    df_pfor = pd.read_csv(dir_ratio_pfor)
+    df_pfor['Encoding Algorithm'] = df_pfor['Encoding Algorithm'].replace('REGER', 'REGER (our)')
+    for k in range(df_pfor.shape[0]):
+        df_pfor.loc[k,"Encode"] = df_pfor.iloc[k,2] / df_pfor.iloc[k,4]
+    df_avg_pfor = df_pfor.groupby(by = df_pfor['Encoding Algorithm'], as_index=False).agg(lambda x: x.mean() if pd.api.types.is_numeric_dtype(x) else x)
+    len_pfor_df = df_avg_pfor.shape[0]
+
+
+
+
+    for i in range(len_6_df):
+        cr = df_avg.iloc[i,-1]
+        df_result.loc[result_count] = [df_avg.iloc[i,0],dir_r[j],cr]
+        result_count += 1
+
+    
+    for i in range(len_rr_df):
+        cr = df_avg_rr.iloc[i,-1]
+        df_result.loc[result_count] = [df_avg_rr.iloc[i,0],dir_r[j],cr]
+        result_count += 1        
+
+
+    for i in range(len_pfor_df):
+        cr = df_avg_pfor.iloc[i,-1]
+        df_result.loc[result_count] = [df_avg_pfor.iloc[i,0],dir_r[j],cr]
+        result_count += 1
+
+
+
+
+df_result.to_csv("./compression_ratio/encode_time.csv",index=False)
+
+# dir_r = ["EPM-Education","GW-Magnetic","Metro-Traffic", "Nifty-Stocks", "USGS-Earthquakes", "Vehicle-Charge","CS-Sensors", "Cyber-Vehicle", "TH-Climate", "TY-Fuel","TY-Transport","YZ-Electricity"]
+
+df_result = pd.DataFrame(columns=['Encoding','Dataset','Decoding Time'])
+result_i = 0
+
+for j in range(len(dir_r)):
+
+    dir_ratio = path_ratio + dir_r[j] + "_ratio.csv"
+    df = pd.read_csv(dir_ratio)
+    for k in range(df.shape[0]):
+        df.loc[k,"Decode"] = df.iloc[k,5] / df.iloc[k,6]
+    df_avg = df.groupby(by = df['Encoding Algorithm'], as_index=False).agg(lambda x: x.mean() if pd.api.types.is_numeric_dtype(x) else x)
+    len_6_df = df_avg.shape[0]
+
+
+    dir_ratio_rr = path_rr_ratio + dir_r[j] + "_ratio.csv"
+    df_rr = pd.read_csv(dir_ratio_rr)
+    df_rr['Encoding Algorithm'] = df_rr['Encoding Algorithm'].replace('ElfCompressor', 'Elf')
+    for k in range(df_rr.shape[0]):
+        df_rr.loc[k,"Decode"] = df_rr.iloc[k,3] / 1000 #df_rr.iloc[k,4] 
+    df_avg_rr = df_rr.groupby(by = df_rr['Encoding Algorithm'], as_index=False).agg(lambda x: x.mean() if pd.api.types.is_numeric_dtype(x) else x)
+    len_rr_df = df_avg_rr.shape[0]
+
+    dir_ratio_pfor = path_remove_value + dir_r[j] + "_ratio.csv"
+    df_pfor = pd.read_csv(dir_ratio_pfor)
+    df_pfor['Encoding Algorithm'] = df_pfor['Encoding Algorithm'].replace('REGER', 'REGER (our)')
+    for k in range(df_pfor.shape[0]):
+        df_pfor.loc[k,"Encode"] = df_pfor.iloc[k,3] / df_pfor.iloc[k,4]
+    df_avg_pfor = df_pfor.groupby(by = df_pfor['Encoding Algorithm'], as_index=False).agg(lambda x: x.mean() if pd.api.types.is_numeric_dtype(x) else x)
+    len_pfor_df = df_avg_pfor.shape[0]
+
+
+
+    for i in range(len_6_df):
+        cr = df_avg.iloc[i,-1]
+        df_result.loc[result_i] = [df_avg.iloc[i,0],dir_r[j],cr]
+        result_i += 1
+    
+
+    for i in range(len_rr_df):
+        cr = df_avg_rr.iloc[i,-1]
+        df_result.loc[result_i] = [df_avg_rr.iloc[i,0],dir_r[j],cr]
+        result_i += 1
+
+    
+    for i in range(len_pfor_df):
+        cr = df_avg_pfor.iloc[i,-1]
+        df_result.loc[result_count] = [df_avg_pfor.iloc[i,0],dir_r[j],cr]
+        result_count += 1
+        
+
+df_result.to_csv("./compression_ratio/decode_time.csv",index=False)
---
+++ draw_time.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+sns.set_theme(style="ticks", palette="pastel")
+my_palette1 = ["#1178b4", "#33a02c", "#ff7f00","#6a3d9a","#fb9a99", "#814a19","#9dafff","#e31a1c","#333333"]
+dataset_list = ["EPM-Education","GW-Magnetic","Metro-Traffic", "Nifty-Stocks", "USGS-Earthquakes", "CS-Sensors", 
+                "Cyber-Vehicle", "TH-Climate", "TY-Fuel","TY-Transport","FANYP-Sensors","TRAJET-Transport"] 
+encoding_list = ["RLE","SPRINTZ","GORILLA","CHIMP","Elf","BUFF","TS_2DIFF","REGER (our)"]
+
+
+df = pd.read_csv("./compression_ratio/encode_time.csv")
+
+
+size = 20
+
+fig, ax_arr = plt.subplots(1,1, figsize=(28,5))
+
+f = sns.barplot(x="Dataset", y="Encoding Time",
+            order = dataset_list,
+            hue="Encoding",
+            hue_order=encoding_list,
+            palette=my_palette1,
+            data=df,
+            ax = ax_arr)
+ax_arr.set_yscale("log")
+f.set_xticklabels(labels = dataset_list,  rotation=10)
+ax_arr.get_legend().remove()
+f.tick_params(labelsize = size)
+f.xaxis.label.set_size(size)
+f.yaxis.label.set_size(size)
+f.set_ylim(0.9,9000)
+f.set_ylabel("Encoding Time ns/points")
+
+lines, labels = ax_arr.get_legend_handles_labels()
+fig.legend(lines, labels, bbox_to_anchor=(0.5, 0.9),loc = 'upper center',fontsize=size,handletextpad=0.6,labelspacing=0.8,columnspacing =0.8, ncol=8)
+
+
+fig.savefig("./figs/vary_dataset_time.eps",format='eps',dpi = 400,bbox_inches='tight')
+fig.savefig("./figs/vary_dataset_time.png", dpi = 400,bbox_inches='tight')
+
+df = pd.read_csv("./compression_ratio/decode_time.csv")
+
+
+size = 20
+
+
+fig, ax_arr = plt.subplots(1,1, figsize=(28,5))
+f = sns.barplot(x="Dataset", y="Decoding Time",
+            order = dataset_list,
+            hue="Encoding",
+            hue_order=encoding_list,
+            palette=my_palette1,
+            data=df,
+            ax = ax_arr)
+ax_arr.set_yscale("log")
+f.set_xticklabels(labels = dataset_list,  rotation=10)
+ax_arr.get_legend().remove()
+f.tick_params(labelsize = size)
+f.xaxis.label.set_size(size)
+f.yaxis.label.set_size(size)
+f.set_ylabel("Decoding Time ns/points")
+f.set_ylim(0.9,9000)
+
+lines, labels = ax_arr.get_legend_handles_labels()
+fig.legend(lines, labels, bbox_to_anchor=(0.5, 0.9),loc = 'upper center',fontsize=size,handletextpad=0.6,labelspacing=0.8,columnspacing =0.8, ncol=8)
+
+
+fig.savefig("./figs/vary_dataset_time_decode.eps",format='eps',dpi = 400,bbox_inches='tight')
+fig.savefig("./figs/vary_dataset_time_decode.png", dpi = 400,bbox_inches='tight')
